60b66d1ca0b1a950f34f693aa01ad636fa1cb84d
Raditya Ananda Putra <Radityap2107@gmail.com>
2026-04-28 20:37:43 +0700

    fix: Undangan Wa

--- app/generator/page.tsx
+++ app/generator/page.tsx
@@ -1,46 +1,11 @@
-"use client";
+import WeddingContainer from "@/components/wedding/WeddingContainer";
 
-import React, { useState } from "react";
+export default function Page({
+  searchParams,
+}: {
+  searchParams?: { to?: string };
+}) {
+  const guestName = searchParams?.to ?? "";
 
-export default function LinkGenerator() {
-  const [name, setName] = useState("");
-
-  const baseUrl =
-    "https://wedding-invitation-radityaputra.vercel.app/invitation";
-
-  const slugify = (str: string) =>
-    str.trim().toLowerCase().replace(/\s+/g, "-");
-
-  const finalLink = `${baseUrl}?to=${encodeURIComponent(slugify(name))}`;
-
-  const copyToClipboard = async () => {
-    if (!name) return;
-    await navigator.clipboard.writeText(finalLink);
-    alert("Link berhasil disalin!");
-  };
-
-  const shareToWhatsApp = () => {
-    if (!name) return;
-
-    const text = `Tanpa mengurangi rasa hormat, kami mengundang Bapak/Ibu/Saudara/i *${name}* untuk hadir di acara pernikahan kami.
-
-Detail undangan:
-${finalLink}`;
-
-    const waUrl = `https://api.whatsapp.com/send?text=${encodeURIComponent(text)}`;
-    window.open(waUrl, "_blank");
-  };
-
-  return (
-    <div>
-      <input
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        placeholder="Nama tamu"
-      />
-
-      <button onClick={shareToWhatsApp}>Kirim WA</button>
-      <button onClick={copyToClipboard}>Copy Link</button>
-    </div>
-  );
+  return <WeddingContainer guestName={guestName} />;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
-// app/invitation/page.tsx
-
+import { Metadata } from "next";
 import WeddingContainer from "@/components/wedding/WeddingContainer";
+
+export const metadata: Metadata = {
+  title: "Undangan Pernikahan - Radit & Keiani",
+  description:
+    "Tanpa mengurangi rasa hormat, kami mengundang Anda untuk hadir di acara pernikahan kami.",
+};
 
 export default function Page({
   searchParams,
 }: {
-  searchParams: { to?: string };
+  searchParams?: { to?: string };
 }) {
-  const guestName = searchParams?.to ?? "";
+  const guestName = searchParams?.to ?? "Tamu Undangan";
 
   return <WeddingContainer guestName={guestName} />;
 }
