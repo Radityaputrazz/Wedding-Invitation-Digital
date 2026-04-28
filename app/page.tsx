4bebd46668c51e900df72b7f078b89d6d3e03ae8
Raditya Ananda Putra <Radityap2107@gmail.com>
2026-04-28 20:42:13 +0700

    fix: Undangan Wa

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
+import { Metadata } from "next";
 import WeddingContainer from "@/components/wedding/WeddingContainer";
+
+export const metadata: Metadata = {
+  title: "Undangan Pernikahan - Radit & Keiani",
+  description:
+    "Tanpa mengurangi rasa hormat, kami mengundang Anda untuk hadir di acara pernikahan kami.",
+};
 
 export default function Page({
   searchParams,
 }: {
   searchParams?: { to?: string };
 }) {
-  return (
-    <WeddingContainer guestName={searchParams?.to ?? "Tamu Undangan"} />
-  );
+  const guestName = searchParams?.to ?? "Tamu Undangan";
+
+  return <WeddingContainer guestName={guestName} />;
 }
