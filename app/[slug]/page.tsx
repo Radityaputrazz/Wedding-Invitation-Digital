6d718336fe92cee073427b689b63fd0de942d3f8
Raditya Ananda Putra <Radityap2107@gmail.com>
2026-04-28 20:51:00 +0700

    fix: Undangan Wa

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,13 +1,7 @@
-import WeddingContainer from "@/components/wedding/WeddingContainer";
-
-export default function Page({
-  params,
-}: {
-  params: { slug: string };
-}) {
+export default function Page({ params }: { params: { slug: string } }) {
   const guestName = params.slug
     .replace(/-/g, " ")
     .replace(/\b\w/g, (c) => c.toUpperCase());
 
-  return <WeddingContainer guestName={guestName} />;
+  return <div>{guestName}</div>;
 }
